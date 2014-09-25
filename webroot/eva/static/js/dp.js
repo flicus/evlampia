d8dc16d369de4bf741c65b747ccfbc1f45007288
flicus <flicus@gmail.com>
2014-09-25 20:08:52 +0400

    test target

--- webroot/eva/static/js/dp.js
+++ webroot/eva/static/js/dp.js
@@ -2,7 +2,25 @@
 //
 
 $(document).ready(function () {
-    $('#listRender').datepick({monthsToShow: [3, 4], monthsToStep:12, monthsOffset:function(date){return date.getMonth();}, showOtherMonths: false, changeMonth: true, onShow: $.datepick.showStatus});
+    $('#listRender')
+        .datepick({
+            monthsToShow: [3, 4],
+            monthsToStep: 1,
+            monthsOffset: function (date) {
+                return date.getMonth();
+            },
+            showOtherMonths: false,
+            changeMonth: true,
+            onShow: $.datepick.showStatus,
+            minDate: new Date(2011, 8 - 1, 23),
+            maxDate: new Date(),
+            onSelect: function (date) {
+                var day = date[0].getDate() < 10 ? '0' + date[0].getDate() : date[0].getDate();
+                var month = date[0].getMonth() + 1 < 10 ? '0' + (date[0].getMonth() + 1) : date[0].getMonth() + 1;
+                var url = 'http://0xffff.net/logs/vnations@conference.jabber.ru/' + date[0].getFullYear() + '/' + month + '/' + day;
+                window.location = url;
+            }
+        });
 
 });
 
