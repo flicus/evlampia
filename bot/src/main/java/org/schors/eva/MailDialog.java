b2affa9c428e84c4e3576f45d09c1915a6e17a57
schors <flicus@gmail.com>
2016-03-15 10:27:00 +0300

    messageId -> int

--- bot/src/main/java/org/schors/eva/MailDialog.java
+++ bot/src/main/java/org/schors/eva/MailDialog.java
@@ -1,7 +1,38 @@
 package org.schors.eva;
 
-/**
- * Created by flicus on 15.03.16.
- */
 public class MailDialog {
+    private String email;
+    private MailState state;
+
+    public MailDialog() {
+    }
+
+    public MailDialog(MailState state, String email) {
+        this.state = state;
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public MailState getState() {
+        return state;
+    }
+
+    public void setState(MailState state) {
+        this.state = state;
+    }
+
+    @Override
+    public String toString() {
+        return "MailDialog{" +
+                "email='" + email + '\'' +
+                ", state=" + state +
+                '}';
+    }
 }
