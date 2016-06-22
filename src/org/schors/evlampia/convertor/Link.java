8d8225ab64c15c81f0ad0f59ebe62d23d192fba5
schors <flicus@gmail.com>
2016-06-22 13:02:32 +0300

    Update Link.java

--- src/org/schors/evlampia/convertor/Link.java
+++ src/org/schors/evlampia/convertor/Link.java
@@ -2,23 +2,6 @@ package org.schors.evlampia.convertor;
 
 import org.jdom2.Element;
 
-/**
- * Copyright (c) 2013 Amdocs jNetX.
- * http://www.amdocs.com
- * All rights reserved.
- * <p/>
- * This software is the confidential and proprietary information of
- * Amdocs jNetX. You shall not disclose such Confidential Information and
- * shall use it only in accordance with the terms of the license
- * agreement you entered into with Amdocs jNetX.
- * <p/>
- * User: Sergey Skoptsov (sskoptsov@amdocs.com)
- * Date: 26.09.2014
- * Time: 18:48
- * <p/>
- * $Id:
- */
-
 public class Link implements Convertable {
 
     private String link;
