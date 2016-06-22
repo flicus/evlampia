3bd3894e0e656fd76ba9235eed752d76b375c486
schors <flicus@gmail.com>
2016-06-22 13:02:58 +0300

    copyleft

--- src/org/schors/evlampia/convertor/Record.java
+++ src/org/schors/evlampia/convertor/Record.java
@@ -5,23 +5,6 @@ import org.jdom2.Element;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-/**
- * Copyright (c) 2013 Amdocs jNetX.
- * http://www.amdocs.com
- * All rights reserved.
- * <p/>
- * This software is the confidential and proprietary information of
- * Amdocs jNetX. You shall not disclose such Confidential Information and
- * shall use it only in accordance with the terms of the license
- * agreement you entered into with Amdocs jNetX.
- * <p/>
- * User: Sergey Skoptsov (sskoptsov@amdocs.com)
- * Date: 26.09.2014
- * Time: 17:04
- * <p/>
- * $Id:
- */
-
 public class Record implements Convertable {
     private static final Pattern pattern = Pattern.compile("<a\\s+target=\"_blank\"\\s+href='(.*?)'>.*?</a>", Pattern.MULTILINE);
 
