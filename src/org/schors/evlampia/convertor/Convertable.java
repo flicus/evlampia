c1a01fdf6597d74f16f03fcdf877d2bf6f68e5ca
schors <flicus@gmail.com>
2016-06-22 13:01:36 +0300

    copyleft

--- src/org/schors/evlampia/convertor/Convertable.java
+++ src/org/schors/evlampia/convertor/Convertable.java
@@ -2,23 +2,6 @@ package org.schors.evlampia.convertor;
 
 import org.jdom2.Element;
 
-/**
- * Copyright (c) 2013 Amdocs jNetX.
- * http://www.amdocs.com
- * All rights reserved.
- * <p/>
- * This software is the confidential and proprietary information of
- * Amdocs jNetX. You shall not disclose such Confidential Information and
- * shall use it only in accordance with the terms of the license
- * agreement you entered into with Amdocs jNetX.
- * <p/>
- * User: Sergey Skoptsov (sskoptsov@amdocs.com)
- * Date: 26.09.2014
- * Time: 17:09
- * <p/>
- * $Id:
- */
-
 public interface Convertable {
 
     public Element toElement();
